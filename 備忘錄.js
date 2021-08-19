204fc27734d9b35830dbac25a4eb6ead62736ef3
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-19 15:41:31 +0800

    Create 備忘錄.js

---
+++ 備忘錄.js
@@ -0,0 +1,13 @@
+// 備忘錄ＡＰＰ
+const content = document.getElementById('content');
+const data = document.getElementById('data');
+const time = document.getElementById('tome');
+const addedBtn = document.getElementById('addedBtn');
+const deletedBen = document.getElementById('deletedBen');
+
+
+addedBtn.addEventListener('click'.function(){
+                          console.log(content.value)
+    console.log(data.value)
+    console.log(time.value)
+});
