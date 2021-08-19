b0a8e6a90fb8e41e7348b426ef5d3ead184413ac
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-19 15:39:01 +0800

    Create process.js

---
+++ process.js
@@ -0,0 +1,21 @@
+let degree= 38;
+// console.log(degree >=37.5);
+if (degree>=37.5){
+  console.log("您不得進入");
+}else if(degree<37.5){
+  console.log(您可以進入);
+}
+
+let score = 90;
+if(score<=60){
+  console.log("您的成績不及格");
+}else{console.log("您的成績及格")}
+
+
+let myscroe = 88;
+if (myscroe>=80){
+  console.log("優秀區間");
+}else if(myscore >=60){
+  console.log("及格線以上區間")
+}else{
+      console.log("需要再努力")}
