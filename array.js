f28644d8f8151857d57001fa7c016a80f7c93cb4
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-19 15:37:21 +0800

    Create array.js

---
+++ array.js
@@ -0,0 +1,15 @@
+// 陣列
+let colors=['red','yellow','black'];
+// 物件
+let home={
+  "fatherName":"Tom",
+  "motherName":"Mary",
+  "sons":["Bob","John","Tim"],
+  "isWakeUp":false,
+  "dogs":3,
+}
+// 也能寫成空陣列
+let ary=[];
+let books=[5,30,400,100];
+// 陣列裡頭可以放各式各樣的資料
+console.log(books);
