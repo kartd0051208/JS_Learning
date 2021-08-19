bc0393155f1814ec866173b504e45e3332da998a
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-19 15:43:00 +0800

    Create compare.js

---
+++ compare.js
@@ -0,0 +1,18 @@
+let e= 10;
+let f= 5 ;
+
+console.log(e!=f);
+// 可以做變數的比較。
+// 比較運算子。
+
+let k = 2>=1;
+console.log(k);
+
+let isWakeUp = false;
+console.log(isWakeUp);
+
+let a = 2 ; 
+let b = 3 ;
+console.log((a+b)>1);
+
+
