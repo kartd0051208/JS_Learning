4dad13da2c10666aa63d212e3eca913130d01c88
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-26 17:48:59 +0800

    Create array(unshift).js

---
+++ array(unshift).js
@@ -0,0 +1,25 @@
+// unshift 不移動加入，且加入資料
+
+// let colors =['blue','pink','black'];
+
+// // unshift會直接加入到 第0筆資料
+// colors.unshift('red');
+
+// console.log(colors);
+
+// 新增 push, unshift
+// let Acolors =['blue','pink','black'];
+// // 刪除語法 pop,shift
+// Acolors.pop();
+// console.log(Acolors);
+// pop 會刪處最後一筆資料
+
+
+// shift
+let Bcolors=['blue','pink','black'];
+Bcolors.shift();
+console.log(Bcolors);
+// shift 會刪除第0筆資料
+
+// shift 往前面刪除,pop往後面刪除
+// unshift 往前面新增 ,push往後面新增
