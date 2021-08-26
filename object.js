2c85a4c2351d32ab4fa031d905b0ce8d086f0bcf
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-26 17:46:23 +0800

    Create object.js

---
+++ object.js
@@ -0,0 +1,16 @@
+// object物件
+// 描述家庭
+// 宣告一個變數，讓這些資料保存在物件裡面。
+let family={
+  dadName:"Eli",
+  momName:"amy",
+  dogs:3,
+  isWakeUp:false,
+}
+let a = family.dadName;
+console.log(family.momName);
+// . 是尋找/指向屬性
+console.log(a);
+// 裡頭是屬性，對應的就是vlaue的值
+
+
