562268fbad9442888afac224114f3c7dcf598043
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-08-30 12:38:43 +0800

    Update return.js

--- return.js
+++ return.js
@@ -1,1 +1,12 @@
+function calcTotalScore(chineseScore,mathScore){
+  // console.log(chineseScore+mathScore)};
+  return chineseScore + mathScore;
+}
 
+// console.log 只是把執行的值印出來，並沒有回傳出來。
+
+// return 的意思就是回傳，把東西給回傳出來。
+
+let personTotalScore= calcTotalScore(33,77);
+
+console.log(personTotalScore);
